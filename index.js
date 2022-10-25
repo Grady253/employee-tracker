3bf882029d615d97964d153e8455f732516ff623
Grady253 <grady.andre2@gmail.com>
2022-10-24 21:58:23 -0700

    created some started files.

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mysql = require('mysql2');
+
+
+
+
+// view all department 
+
+//SELECT * FROM department 
+
+//view all roles
+
+//SELECT * FROM ROLE
+
+//view all employees
+
+//SELECT * EMPLOYEE
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
